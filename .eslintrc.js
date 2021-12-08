64216b66c63ddfa91135987c6e4601c0a128dbda
akijoey <akijoey@akijoey.com>
2021-12-08 19:54:48 +0800

    fix: upgrade eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,12 @@
 // .eslintrc.js
 
 module.exports = {
-  extends: ['standard', 'prettier', 'prettier/standard'],
+  extends: ['standard', 'standard-jsx', 'standard-with-typescript', 'prettier'],
   env: {
     node: true,
     jest: true
+  },
+  parserOptions: {
+    project: 'tsconfig.json'
   }
 }
