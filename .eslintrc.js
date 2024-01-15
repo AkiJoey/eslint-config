78795580455d03ec1fdd0525cf0e375966e2eedd
akijoey <akijoey@akijoey.com>
2024-01-15 06:40:23 +0000

    feat: override typescript parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,18 @@
 // .eslintrc.js
 
+const extend = config =>
+  ['standard', 'standard-jsx', config, 'prettier'].flat().filter(Boolean)
+
 module.exports = {
-  extends: ['standard', 'standard-jsx', 'standard-with-typescript', 'prettier'],
+  extends: extend(),
   env: {
     node: true,
     jest: true
   },
-  parserOptions: {
-    project: 'tsconfig.json'
-  }
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: extend('standard-with-typescript')
+    }
+  ]
 }
